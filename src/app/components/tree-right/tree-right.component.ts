f98a20bae2a5bea1548a8d546f26c3f33d633bca
nhatkhanh0204 <nhatkhanh0204@gmail.com>
2022-02-23 15:02:12 +0700

    Demo-UI-fix: Chỉnh sửa giao diện cho treeview và popup

--- src/app/components/tree-right/tree-right.component.ts
+++ src/app/components/tree-right/tree-right.component.ts
@@ -8,91 +8,118 @@ import { Component, OnInit, ɵɵqueryRefresh } from '@angular/core';
   styleUrls: ['./tree-right.component.css']
 })
 export class TreeRightComponent implements OnInit {
+
   treeViews = [
-    {
-      room: 'Phòng Tài chính Kế toán',
-      code: 'TCKT',
-      mission: 'Quản trị tài chính DN & đầu tư'
-    },
+      {
+        room: 'Hệ thống tư vấn quản lý trực tập',
+        code: 'Mã',
+        mission: 'Mô tả chức năng nhiệm vụ',
+        spaceOne: [
+          {
+            room: 'Giám đốc',
+            code: 'BGD-1',
+            mission: 'Điều hành hoạt động doanh nghệp',
+            spaceTwo:[
+              {
+                room: 'Phó giám đốc Phụ trách kinh doanh',
+                code: 'BGD-2',
+                mission: 'Điều hành hoạt động doanh nghệp',
+                font: 'bi bi-chevron-down',
+                spaceThree:[
+                  {
+                    room: 'Phòng phát triển thị trường',
+                    code: 'PTTT',
+                    mission: 'Marketting',
+                    space:[]
+                  },
 
-    {
-      room: 'Phòng Quản trị Nguồn nhân lực',
-      code: 'QTNNL',
-      mission: 'Xây dựng hệ thống & các'
-    },
+                  {
+                    room: 'Phòng Kinh doanh',
+                    code: 'KD',
+                    mission: 'Mô tả chung...',
+                    space:[]
+                  }
+                ]
+              },
 
-    {
-      room: 'Phòng Marketing',
-      code: 'MK',
-      mission: 'Mô tả chung'
-    },
+              {
+                room: 'Phòng Tài chính Kế toán',
+                code: 'TCKT',
+                mission: 'Quản trị Tài chính',
 
-    {
-      room: 'Phòng R&D',
-      code: 'RD',
-      mission: 'Mô tả chung'
-    },
+              },
 
-    {
-      room: 'Phòng Hành chính tổng hợp',
-      code: 'HC',
-      mission: 'Mô tả chung'
-    },
+              {
+                room: 'Phòng Quản trị nguồn nhân lực',
+                code: 'QTNNL',
+                mission: 'Xây dựng hệ thống & các chính sách',
 
-    {
-      room: 'Phòng Kỹ thuật',
-      code: 'BGD-1-7',
-      mission: 'Mô tả chung'
-    }
-  ]
+              },
 
-  treeSmalls = [
-    {
-      room: 'Phòng phát triển thị trường',
-      code: 'PTTT',
-      mission: 'Marketing, chính sách giá'
-    },
+              {
+                room: 'Phòng Marketing',
+                code: 'MK',
+                mission: 'Mô tả chung...',
 
-    {
-      room: 'Phòng Kinh doanh',
-      code: 'KD',
-      mission: 'Mô tả chung'
-    }
+              },
 
-  ]
+              {
+                room: 'Phòng R&D',
+                code: 'RD',
+                mission: 'Mô tả chung...',
 
-  fiveButtons = [
-    {
-      name: 'Refresh',
-      icon: 'bi bi-arrow-clockwise',
-      color: '#519eb5'
-    },
-    {
-      name: 'Thêm mới',
-      margins: '5px',
-      icon: 'bi bi-plus',
-      color: '#2aa843'
-    },
-    {
-      name: 'Xuất file Excel',
-      icon: 'bi bi-file-earmark-excel',
-      color: '#2aa843'
-    },
-    {
-      name: 'Tải file mẫu',
-      icon: 'bi bi-cloud-arrow-down-fill',
-      color: '#000000'
-    },
-    {
-      name: 'Import',
-      icon: 'bi bi-arrow-bar-up',
-      color: '#a93226'
-    }
-  ]
+              },
 
-  buttoms = [
+              {
+                room: 'Phòng Hành chính tổng hợp',
+                code: 'HC',
+                mission: 'Mô tả chung...',
 
-  ]
+              },
+
+              {
+                room: 'Phòng Kỹ thuật',
+                code: 'BGD-1-7',
+                mission: 'Mô tả chung...',
+
+              }
+
+            ]
+          }
+        ]
+      },
+    ]
+
+
+    fiveButtons = [
+        {
+          name: 'Refresh',
+          icon: 'bi bi-arrow-clockwise',
+          color: '#519eb5'
+        },
+        {
+          name: 'Thêm mới',
+          margins: '5px',
+          icon: 'bi bi-plus-lg',
+          color: '#2aa843'
+        },
+        {
+          name: 'Xuất file Excel',
+          icon: 'bi bi-file-earmark-excel',
+          color: '#2aa843'
+        },
+        {
+          name: 'Tải file mẫu',
+          icon: 'bi bi-cloud-arrow-down-fill',
+          color: '#000000'
+        },
+        {
+          name: 'Import',
+          icon: 'bi bi-arrow-bar-up',
+          color: '#a93226'
+        }
+      ]
+
   constructor() { }
 
   ngOnInit(): void {
